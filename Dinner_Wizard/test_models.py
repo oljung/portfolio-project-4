c30ecb48085cb01e62f99e3d2bf09d2c67efb844
oljung <patrik_osterljung@hotmail.com>
2022-01-24 15:32:28 +0000

    fix: additional fixes to models to acces testing of postgres

--- Dinner_Wizard/test_models.py
+++ Dinner_Wizard/test_models.py
@@ -1,20 +1,54 @@
 from django.test import TestCase
-from .models import Ingredient, IngredientTemplate
+from .models import Ingredient, IngredientTemplate, Recipe
 
 
 class TestIngredients(TestCase):
     """
     Test the ingredient models Ingredient and IngredientTemplate
     """
-    
+
     def test_template_str_function_returns_name(self):
         template = IngredientTemplate.objects.create(name='Salt', unit='ml')
         self.assertEqual(str(template), 'Salt')
-    
+
     def test_ingredient_str_function_returns_name(self):
-        ingredient = Ingredient.objects.create(name='Salt', quantity=5, unit='ml')
+        ingredient = Ingredient.objects.create(
+            name='Salt',
+            quantity=5,
+            unit='ml'
+        )
         self.assertEqual(str(ingredient), 'Salt')
-    
+
     def test_ingredient_quantity_is_float(self):
-        ingredient = Ingredient.objects.create(name='Salt', quantity=5, unit='ml')
+        ingredient = Ingredient.objects.create(
+            name='Salt',
+            quantity=5,
+            unit='ml'
+        )
         self.assertEqual(ingredient.quantity, 5.0)
+
+
+class TestRecipe(TestCase):
+    """
+    Test parts of recipe model
+    """
+    recipe = Recipe.objects.create(
+        name='Test',
+        created_by='User',
+        description='Just to test the model',
+    )
+    def test_str_function_returns_name(self):
+        self.assertEqual(str(TestRecipe.recipe), 'Test')
+
+    def test_ingredients_is_array(self):
+        ingredient = Ingredient.objects.create(
+            name='Salt',
+            quantity=5,
+            unit='ml',
+        )
+        TestRecipe.recipe.ingredients.add(ingredient)
+        self.assertEqual(len(TestRecipe.recipe.ingredients), 1)
+    
+    def test_ingredients_has_correct_data(self):
+        TestRecipe.recipe
+        self.assertEquals(TestRecipe.recipe.ingredients[0].name, 'Salt')
