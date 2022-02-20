3a433f3992c8cf42bdbfb6eed01942ee34203e5b
oljung <patrik_osterljung@hotmail.com>
2022-02-20 18:57:25 +0000

    feat: complete testing for forms

--- Dinner_Wizard/test_forms.py
+++ Dinner_Wizard/test_forms.py
@@ -2,7 +2,13 @@
 Module for testing forms
 """
 from django.test import TestCase
-from .forms import IngredientForm
+from .forms import (
+    IngredientForm,
+    IngredientTemplateForm,
+    PlanForm,
+    RecipeForm,
+    ShoppingListForm
+)
 
 
 # Create your tests here.
@@ -20,3 +26,98 @@ class TestIngredientForm(TestCase):
         self.assertFalse(form.is_valid())
         self.assertIn('quantity', form.errors.keys())
         self.assertEqual(form.errors['quantity'][0], 'This field is required.')
+
+    def test_fields_are_explicit_in_form_metaclass(self):
+        """
+        Test to make sure the metaclass has the necessary fields
+        """
+        form = IngredientForm()
+        self.assertEqual(form.Meta.fields, ['name', 'quantity', 'unit'])
+
+
+class TestIngredientTemplateForm(TestCase):
+    """
+    Test the form for ingredient templates
+    """
+    def test_fields_are_explicit_in_form_metaclass(self):
+        """
+        Test to make sure the metaclass has the necessary fields
+        """
+        form = IngredientTemplateForm()
+        self.assertEqual(form.Meta.fields, ['name', 'unit'])
+
+    def form_has_correct_requirements(self):
+        """
+        Test that form is invalid if data is missing
+        """
+        form = IngredientTemplateForm({'name': 'Salt'})
+        self.assertFalse(form.is_valid())
+        self.assertIn('unit', form.errors.keys())
+        self.assertEqual(form.errors['unit'][0], 'This field is required.')
+
+
+class TestPlansForm(TestCase):
+    """
+    Test the plans form
+    """
+    def test_fields_are_explicit_in_form_metaclass(self):
+        """
+        Test to make sure the metaclass has the necessary fields
+        """
+        form = PlanForm()
+        self.assertEqual(form.Meta.fields, ['name', 'status'])
+
+    def form_has_correct_requirements(self):
+        """
+        Test that form is invalid if data is missing
+        """
+        form = PlanForm({'status': 1})
+        self.assertFalse(form.is_valid())
+        self.assertIn('name', form.errors.keys())
+        self.assertEqual(form.errors['name'][0], 'This field is required.')
+
+
+class TestRecipeForm(TestCase):
+    """
+    Test the recipe form
+    """
+    def test_fields_are_explicit_in_form_metaclass(self):
+        """
+        Test to make sure the metaclass has the necessary fields
+        """
+        form = RecipeForm()
+        self.assertEqual(form.Meta.fields, ['name', 'description'])
+
+    def form_has_correct_requirements(self):
+        """
+        Test that form is invalid if data is missing
+        """
+        form = RecipeForm({'name': 'Test'})
+        self.assertFalse(form.is_valid())
+        self.assertIn('description', form.errors.keys())
+        self.assertEqual(
+            form.errors['description'][0], 'This field is required.'
+        )
+
+
+class TestShoppingListForm(TestCase):
+    """
+    Test the shopping list form
+    """
+    def test_fields_are_explicit_in_form_metaclass(self):
+        """
+        Test to make sure the metaclass has the necessary fields
+        """
+        form = ShoppingListForm()
+        self.assertEqual(form.Meta.fields, ['name'])
+
+    def form_has_correct_requirements(self):
+        """
+        Test that form is invalid if data is missing
+        """
+        form = ShoppingListForm()
+        self.assertFalse(form.is_valid())
+        self.assertIn('name', form.errors.keys())
+        self.assertEqual(
+            form.errors['name'][0], 'This field is required.'
+        )
